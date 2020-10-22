49b509b6126d9bd9acb8c0289660ebc7ad6d913d
Rishabh Gupt <dashingrishigupta@gmail.com>
2020-10-22 21:33:12 +0530

    Zig-Zag

---
+++ Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    public GameObject platform;
+    public GameObject diamonds;
+    Vector3 lastPos;
+    float size;
+    public bool gameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPos = platform.transform.position;
+        size = platform.transform.localScale.x;
+
+        for(int i=0; i<20; i++) {
+            SpawnPlatforms();
+        }
+    }
+
+    public void StartSpawningPlatforms() {
+        InvokeRepeating("SpawnPlatforms", 0.1f, 0.2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameManager.instance.gameOver) {
+            CancelInvoke("SpawnPlatforms");
+        }
+    }
+
+    void SpawnPlatforms() {
+        int rand = Random.Range(0, 6);
+        if(rand < 3) {
+            SpawnX();
+        } else {
+            SpawnZ();
+        }
+    }
+
+    void SpawnX() {
+        Vector3 pos = lastPos;
+        pos.x += size;
+        lastPos = pos;
+        // Instantiating Platform here(Randomly creating platform)
+        Instantiate(platform, pos, Quaternion.identity);
+        
+        int rand = Random.Range(0,4);
+        if(rand<1) {
+            Instantiate(diamonds, new Vector3(pos.x, pos.y+1, pos.z), diamonds.transform.rotation);
+        }
+    }
+
+    void SpawnZ() {
+        Vector3 pos = lastPos;
+        pos.z += size;
+        lastPos = pos;
+        // Instantiating Diamonds here(Randomly creating diamonds)
+        Instantiate(platform, pos, Quaternion.identity);
+        
+        int rand = Random.Range(0,4);
+        if(rand<1) {
+            Instantiate(diamonds, new Vector3(pos.x, pos.y+1, pos.z), diamonds.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    public GameObject zigzagPanel;
+    public GameObject gameOverPanel;
+    public GameObject tapText;
+    public Text score;
+    public Text highScore1;
+    public Text highScore2;
+
+    void Awake() {
+        if(instance == null) {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        highScore1.text = "High Scores : " + PlayerPrefs.GetInt("highScore");
+    }
+
+    public void GameStart() {
+        tapText.SetActive(false);
+        zigzagPanel.GetComponent<Animator>().Play("panelUp");
+    }
+
+    public void GameOver() {
+        score.text = PlayerPrefs.GetInt("score").ToString();
+        highScore2.text = PlayerPrefs.GetInt("highScore").ToString();
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Replay() {
+        SceneManager.LoadScene(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
